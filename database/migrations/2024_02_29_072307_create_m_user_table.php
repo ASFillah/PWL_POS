1ff1852cfb3dda3ccfda6ca1529928da286e1fe1
Fillah Amanda Sulton <2131710001@student.polinema.ac.id>
2024-02-29 15:02:51 +0700

    Added all databases

---
+++ database/migrations/2024_02_29_072307_create_m_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('m_user', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->unsignedBigInteger('level_id')->index();
+            $table->foreign('level_id')->references('level_id')->on('m_level');
+            $table->string('username', 20);
+            $table->string('nama', 100);
+            $table->string('password', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('m_user');
+    }
+};
