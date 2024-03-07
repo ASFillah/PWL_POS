6c117d74abee778658556de4fe978df7750acbbe
Fillah Amanda Sulton <2131710001@student.polinema.ac.id>
2024-03-07 13:56:48 +0700

    Pertemuan 4, Praktikum 1, $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,20 +12,21 @@ class UserController extends Controller
     {
         //tambah data user dengan Eloquent Model
         /* $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
             'password' => Hash::make('12345'),
-            'level_id' => 4
         ];
-        UserModel::insert($data); //tambahkan data ke tabel <m_user></m_user> */
+        UserModel::create($data); */ //tambahkan data ke tabel <m_user></m_user>
 
 
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
 
         //akses model UserModel
-        $user = UserModel::all();
+        // $user = UserModel::find(1);
+        $user = UserModel::where('level_id', 1)->first();
         return view('user', ['data' => $user]);
     }
 }
